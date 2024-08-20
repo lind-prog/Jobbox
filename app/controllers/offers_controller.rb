6d106c9203624dbf37e7d7bec8bd4b89510b4517
Linda Badji <linda.badji33@gmail.com>
2024-08-21 00:44:00 +0200

    map

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,2 +1,11 @@
 class OffersController < ApplicationController
+  def index
+    @user_job_search = UserJobSearch.all
+    @markers = @user_job_search.geocoded.map do |user_job_search|
+      {
+        lat: user_job_search.latitude,
+        lng: user_job_search.longitude
+      }
+    end
+  end
 end
--- app/controllers/user_job_searches_controller.rb
+++ app/controllers/user_job_searches_controller.rb
@@ -1,5 +1,13 @@
 class UserJobSearchesController < ApplicationController
-
+  def index
+    @user_job_search = UserJobSearch.all
+    @markers = @user_job_search.geocoded.map do |user_job_search|
+      {
+        lat: user_job_search.latitude,
+        lng: user_job_search.longitude
+      }
+    end
+  end
   # def edit
   #   @user_job_search = UserJobSearch.find_or_create(params[:id])
   # end
