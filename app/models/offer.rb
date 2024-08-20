a3108da12cfb3459c1b7bfbc4282bdf6c675ed86
Linda Badji <linda.badji33@gmail.com>
2024-08-20 17:20:08 +0200

    map

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -2,4 +2,6 @@ class Offer < ApplicationRecord
   belongs_to :recruiter, class_name: "User"
   has_many :candidacies
   has_many :matches
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
