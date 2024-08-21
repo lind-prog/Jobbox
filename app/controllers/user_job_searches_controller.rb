e8aa85e1adb75d57f9f6c32d14758aced076fa54
Auriane Durand <aurianed@hotmail.fr>
2024-08-21 17:36:18 +0200

    edit user prof

--- app/controllers/user_job_searches_controller.rb
+++ app/controllers/user_job_searches_controller.rb
@@ -5,11 +5,13 @@ class UserJobSearchesController < ApplicationController
 
   def new
     @user_job_search = UserJobSearch.find_or_create_by(job_seeker: current_user)
+    @user = current_user
   end
 
   def update
+    @user_job_search = UserJobSearch.find(params[:id])
     if @user_job_search.update(user_job_search_params)
-      redirect_to @user_job_search, notice: "Votre profil a été complété avec succès !"
+      redirect_to matches_path, notice: "Votre profil a été complété avec succès !"
     else
       render :edit, status: :unprocessable_entity
     end
