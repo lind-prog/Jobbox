253fd0b82500969e34b30aa444dcf360a7eaacf7
Linda Badji <linda.badji33@gmail.com>
2024-08-21 17:32:05 +0200

    map

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,5 +1,5 @@
 class Offer < ApplicationRecord
-  belongs_to :recruiter, class_name: "User"
+  belongs_to :recruiter, class_name: "User", foreign_key: 'recruiter_id'
   has_many :candidacies
   has_many :matches
   geocoded_by :address
