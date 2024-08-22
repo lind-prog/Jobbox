ea70da19542e62dfc8b2fef6f35f1d5eeb9b1c5c
Auriane Durand <aurianed@hotmail.fr>
2024-08-22 13:04:50 +0200

    validation et dependent destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
   # Relations avec d'autres modèles
-  has_one :user_job_search, foreign_key: :job_seeker_id
-  has_many :candidacies_as_job_seeker, class_name: "Candidacy", foreign_key: :job_seeker_id
-  has_many :messages
+  has_one :user_job_search, foreign_key: :job_seeker_id, dependent: :destroy
+  has_one :offer, foreign_key: :recruiter_id, dependent: :destroy
+  has_many :candidacies_as_job_seeker, class_name: "Candidacy", foreign_key: :job_seeker_id, dependent: :destroy
+  has_many :messages, dependent: :destroy
+  validates :name, presence: true
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
--- app/models/user_job_search.rb
+++ app/models/user_job_search.rb
@@ -1,6 +1,7 @@
 class UserJobSearch < ApplicationRecord
   belongs_to :job_seeker, class_name: "User", foreign_key: :job_seeker_id
-  has_many :matches
+  has_many :matches, dependent: :destroy
   geocoded_by :city
   after_validation :geocode, if: :will_save_change_to_city?
+  validates :title, :contrat_type, :address, :experience_level, :salary, :study_level, :sector, presence: true
 end
