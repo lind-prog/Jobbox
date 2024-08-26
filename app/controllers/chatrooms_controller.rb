88b746351e3f3ddd203726bc5bdb809e1ed2a790
Linda Badji <linda.badji33@gmail.com>
2024-08-26 19:59:08 +0200

    .

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,7 +1,7 @@
 class ChatroomsController < ApplicationController
- # def index
-   # @chatrooms = current_user.chatrooms
-  #end
+  def index
+    @chatrooms = current_user.chatrooms
+  end
 
   # def create
   #   @chatroom = Chatroom.new(chatroom_params)
--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
 
   resources :users, only: %i[show]
   resources :user_job_searches, only: %i[new update create]
-  
+
   resources :user_job_searches, only: %i[show] do
     resources :matches, only: %i[index]
   end
