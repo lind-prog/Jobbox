1390d25cc920384df53f9f98137bff1ddcaa612a
Linda Badji <linda.badji33@gmail.com>
2024-08-26 20:49:18 +0200

    nouvelleseed

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,6 +1,17 @@
 class ChatroomsController < ApplicationController
   def show
     @chatroom = Chatroom.find(params[:id])
-    @message = Message.new
+    @messages = @chatroom.messages.order(created_at: :asc)
+    @message = @chatroom.messages.build
+
+    @other_user = if current_user == @chatroom.candidacy.job_seeker
+                    @chatroom.candidacy.offer.recruiter
+                  else
+                    @chatroom.candidacy.job_seeker
+                  end
+  end
+
+  def index
+    @chatrooms = Chatroom.includes(:messages).all
   end
 end
