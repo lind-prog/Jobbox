0daaabc76b6e702117323273e27e867eef9a3147
lind-prog <linda.badji33@gmail.com>
2024-08-27 09:25:33 +0200

    Update chatrooms_controller.rb

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,17 +1,7 @@
 class ChatroomsController < ApplicationController
   def show
     @chatroom = Chatroom.find(params[:id])
-    @messages = @chatroom.messages.order(created_at: :asc)
-    @message = @chatroom.messages.build
+    @messages = Message.new
 
-    @other_user = if current_user == @chatroom.candidacy.job_seeker
-                    @chatroom.candidacy.offer.recruiter
-                  else
-                    @chatroom.candidacy.job_seeker
-                  end
-  end
-
-  def index
-    @chatrooms = Chatroom.includes(:messages).all
   end
 end
