35ff2a13274e3d3194b705458521a52a1afb9c16
Linda Badji <linda.badji33@gmail.com>
2024-08-27 16:34:15 +0200

    map termine

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,15 +1,22 @@
 class OffersController < ApplicationController
-  def index
-    @offers = UserJobSearch.all
-    @markers = @offers.geocoded.map do |offer|
-      {
-        lat: offer.latitude,
-        lng: offer.longitude
-      }
-    end
-  end
-
   def show
     @offer = Offer.find(params[:id])
+    @match = Match.find_by(offer: @offer, user_job_search: current_user.user_job_search)
+    @marker =
+    [{
+      lat: @offer.latitude,
+      lng: @offer.longitude,
+      marker_html: render_to_string(partial: set_marker(@match))
+    }]
+  end
+
+  def set_marker(match)
+    levels = {
+      "Légendaire" => "matches/markerlegendaire",
+      "Epique" => "matches/markerepique",
+      "Rare" => "matches/marker",
+      "Commun" => "matches/markercommun"
+    }
+    levels[match.level]
   end
 end
--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -5,6 +5,7 @@ export default class extends Controller {
   static values = {
     apiKey: String,
     markers: Array,
+    marker: Array,
     latitude: Number,
     longitude: Number
   }
@@ -16,7 +17,7 @@ export default class extends Controller {
       container: this.element,
       style: "mapbox://styles/titizzy/cm0b493nv00pv01qtglrn5rcw",
       center: [this.longitudeValue, this.latitudeValue],
-      zoom: 16
+      zoom: 17
     })
 
     this.markers = this.markersValue.map(marker => ({
@@ -25,11 +26,16 @@ export default class extends Controller {
       mapMarker: null
     }));
 
-    this.#addMarkersToMap()
-    this.#fitMapToMarkers()
-    this.#addMarkerToOffer()
+    this.marker = this.markerValue.map(marker => ({
+      ...marker,
+      element: this.#createMarkerElement(marker),
+      mapMarker: null
+    }));
 
-    // Attacher les écouteurs d'événements pour les cases à cocher
+    this.#addMarkersToMap()
+    this.#addMarkersToOffer()
+    this.#fitMapToMarkers()
+
     this.element.addEventListener('change', this.#filterMarkers.bind(this));
   }
 
@@ -48,13 +54,15 @@ export default class extends Controller {
         .addTo(this.map);
     });
   }
-  #addMarkerToOffer() {
-    this.markers.forEach(marker => {
+
+  #addMarkersToOffer() {
+    this.marker.forEach(marker => {
       marker.mapMarker = new mapboxgl.Marker(marker.element)
         .setLngLat([ marker.lng, marker.lat ])
         .addTo(this.map);
     });
   }
+
   #fitMapToMarkers() {
     const bounds = new mapboxgl.LngLatBounds();
     this.markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
@@ -63,19 +71,16 @@ export default class extends Controller {
 
   #filterMarkers() {
     const selectedFilters = Array.from(document.querySelectorAll('.filter-checkbox:checked'))
-      .map(checkbox => checkbox.dataset.filter);
+    .map(checkbox => checkbox.dataset.filter);
 
     this.markers.forEach(marker => {
       if (selectedFilters.length === 0 || selectedFilters.includes(marker.type)) {
         marker.mapMarker.getElement().style.display = 'block';
-        // Ensuring marker is added to map in case it was previously removed
         if (!marker.mapMarker.getMap()) {
           marker.mapMarker.addTo(this.map);
         }
       } else {
         marker.mapMarker.getElement().style.display = 'none';
-        // Optionally, you can also remove the marker from the map
-        // marker.mapMarker.remove();
       }
     });
   }
